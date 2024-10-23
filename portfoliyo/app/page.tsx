3d6cd43642df34268f37fd2ec43f73408a48485f
Harshit <richhariyaharshit684@gmail.com>
2024-10-23 11:02:32 +0530

    setup complete

---
+++ portfoliyo/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from '../components/Hero'
+
+export default function Home() {
+  return (
+    <main className="relative bg-blue-950 items-center justify-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
+      <div className="max-w-7xl w-full">
+       <Hero />
+      </div>
+    </main>
+  );
+}
