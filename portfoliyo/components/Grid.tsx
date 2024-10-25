40ed1c96555d3bcad95329386b4e6945b1f3aa6f
Harshit <richhariyaharshit684@gmail.com>
2024-10-25 12:55:07 +0530

    grid section done

--- portfoliyo/components/Grid.tsx
+++ portfoliyo/components/Grid.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/Bentogrid'
 import { i } from 'framer-motion/client'
+import { gridItems } from '@/data'
+import { title } from 'process'
 
 function Grid() {
   return (
     <section id='about'>
       <BentoGrid>
-     {[{ title: 'Title1', description: 'Desc1', id: 1},
-      { title: 'Title1', description: 'Desc1', id: 1},
-      { title: 'Title1', description: 'Desc1', id: 1},
-      { title: 'Title1', description: 'Desc1', id: 1}
-     ].map ((item) => (
+     {gridItems.map ((item) => (
       <BentoGridItem 
       id ={item.id}
       key={item.id}
       title={item.title}
       description={item.description}
+      className={item.className}
+      img ={item.img}
+      imgClassname = {item.titleClassName}
+
+
       />
      ))}
       </BentoGrid>
--- portfoliyo/components/ui/Bentogrid.tsx
+++ portfoliyo/components/ui/Bentogrid.tsx
@@ -23,27 +23,54 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
   icon,
+  header,
+  img,
+  imgClassname,
+  spareImg, 
   id,
+  titleclassname,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
-  id: number,
+  id: number;
+  img?: string;
+  imgClassname?: string;
+  spareImg?: string;
+  titleclassname?: string;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
         className
       )}
+      style={{
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
     >
-      {header}
+
+      <div className={`${id === 6}&& 'flex justify-center h-full'`}>
+        <div className="w-full h-full absolute">
+          <img src={img} alt={img} className={cn(imgClassname, 'object-cover, object-center ')} />
+        </div>
+        <div className={`absolute right-0 bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+          {spareImg && (
+            <img src={spareImg} alt={spareImg} className= {'object-cover, object-center w-full h-full '} />
+          )}
+        </div>
+        <div>
+          {id === 6 && }
+        </div>
+      </div>
+   
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
+        
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
